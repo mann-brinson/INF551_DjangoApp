764ba7266071730bfddab8501fd141b257ee7f76
Mark Mann <mann.brinson@gmail.com>
2020-03-29 16:32:54 -0700

    added dropdown menu options

--- mysite/project/forms.py
+++ mysite/project/forms.py
@@ -3,8 +3,9 @@ from django import forms
 from .models import SearchForm_m
 
 class SearchForm(forms.ModelForm):
-	database = forms.CharField(label='', 
-				widget=forms.TextInput(attrs={"placeholder": "Put database here"}))
+	# database = forms.CharField(label='', 
+	# 			widget=forms.TextInput(attrs={"placeholder": "Put database here"}))
+	database = forms.ChoiceField(choices=[(x, x) for x in ('world', 'kickstarter', 'alumni')])
 	searchterm = forms.CharField(label='', 
 				widget=forms.TextInput(attrs={"placeholder": "Put searchterm here"}))
 	class Meta:
