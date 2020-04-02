e830efcd6d914473eada4d717bb9acd7fa7ffdf2
Mark Mann <mann.brinson@gmail.com>
2020-04-02 12:36:17 -0700

    removed un-needed urls and views now that merged dropdown search with results

--- mysite/project/urls.py
+++ mysite/project/urls.py
@@ -7,23 +7,13 @@ app_name = 'project'
 urlpatterns = [
     path('', views.default, name='default'),
 
-    # ex: /polls/5/
-    # path('<int:question_id>/', views.detail, name='detail'),
-
-    # ex: /polls/5/results/
-    # path('<int:question_id>/results/', views.results, name='results'),
-
-    # ex: /polls/5/vote/
-    # path('<int:question_id>/vote/', views.vote, name='vote'),
-
-
     path('selectdb/', views.selectdb, name='selectdb'),
 
     # ex: /project/sweden/
-    path('<str:database>/', views.db_search, name='database'),
+    #path('<str:database>/', views.db_search, name='database'),
 
     #ex: /project/world/sweden
-    path('<str:database>/<str:searchterm>/', views.db_search, name='searchterm'),
+    #path('<str:database>/<str:searchterm>/', views.db_search, name='searchterm'),
 
     #ex: /project/world/sweden/1
     path('<str:database>/<str:searchterm>/<str:fk_value>/', views.fk_link, name='fk_value'),
