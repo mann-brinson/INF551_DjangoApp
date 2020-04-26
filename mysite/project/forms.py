afcc0439a530ad43b047b1a21f774ad9863bce1e
HankeHJ <hankeheun@gmail.com>
2020-04-25 17:28:18 -0700

    fixed layout of link pages + added link within link + header in link page + back button

--- mysite/project/forms.py
+++ mysite/project/forms.py
@@ -6,8 +6,8 @@ class SearchForm(forms.ModelForm):
 	# database = forms.CharField(label='', 
 	# 			widget=forms.TextInput(attrs={"placeholder": "Put database here"}))
 	database = forms.ChoiceField(choices=[(x, x) for x in ('world', 'kickstarter', 'alumni')])
-	searchterm = forms.CharField(label='', 
-				widget=forms.TextInput(attrs={"placeholder": "Put searchterm here"}))
+	searchterm = forms.CharField(label='Search', 
+				widget=forms.TextInput(attrs={"placeholder": "Enter search term here"}))
 	class Meta:
 		model = SearchForm_m
 		fields = [
